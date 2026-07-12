d1b28f047453d6e2cdd14104b3863c1ae916abec
Rohit123saini <rksaini6398@gmail.com>
2026-07-13 01:25:50 +0530

    new changesrtyd

--- LearnScroll/settings.py
+++ LearnScroll/settings.py
@@ -30,8 +30,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = 'django-insecure-=3#kh$fx$w*-ds==gl98&8+vcsjl=e2!*1^7gg9(l&c%ujhl7s'
 SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False") == "True"
-# DEBUG=True
+# DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG=True
 ALLOWED_HOSTS = ['*','10.93.221.189', 'localhost', '127.0.0.1']
 
 
